cd0853c8350cfef8219592fc3fac16469e76fd93
BhagatAmit <amitbhagat.slg97@gmail.com>
2021-07-19 09:42:13 +0530

    Latest project

---
+++ banking_app/src/test/java/com/banking/service/BankSearchServiceTest.java
@@ -0,0 +1,41 @@
+package com.banking.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import com.banking.model.Login;
+import com.banking.serviceImpl.BankSearchServiceImpl;
+
+
+class BankSearchServiceTest {
+private static BankSearchService service;
+	
+	@BeforeAll
+	public static void setUp() {
+		service=new  BankSearchServiceImpl();
+	}
+	@Test
+	void testGetCustomerByUsername() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetPassWordByUsernameForExisting() {
+		Login login=new Login("Amit%123");
+		login.setUserName("amit123");
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetCustomerByAccountno() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetCurrbalanceByAccountno() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ banking_app_latest- Copy/src/main/java/com/banking/model/User.java
@@ -0,0 +1,80 @@
+package com.banking.model;
+
+public class User {
+	
+	private String userId;
+	private String password;
+	private float balance;
+	
+	public User() {
+		// TODO Auto-generated constructor stub
+	}
+
+	
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public float getBalance() {
+		return balance;
+	}
+
+	public void setBalance(float balance) {
+		this.balance = balance;
+	}
+
+	public User(String userId, String password, float balance) {
+		super();
+		this.userId = userId;
+		this.password = password;
+		this.balance = balance;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + Float.floatToIntBits(balance);
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((userId == null) ? 0 : userId.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (Float.floatToIntBits(balance) != Float.floatToIntBits(other.balance))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (userId == null) {
+			if (other.userId != null)
+				return false;
+		} else if (!userId.equals(other.userId))
+			return false;
+		return true;
+	}
+	
+}
